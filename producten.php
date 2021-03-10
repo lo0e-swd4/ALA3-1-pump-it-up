5819bc3baac922ea994dfc8fa51aa6182102eebb
Daan Berg <6013014@mborijnland.nl>
2021-03-10 13:47:20 +0100

    Begin gemaakt aan de producten pagina

---
+++ producten.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Daan Berg">
+    <meta name="keywords" content="">
+    <title>Producten | Pump It Up</title>
+    <link rel="stylesheet" type="text/css" href="CSS/producten.css">  
+
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="images/logo.png" alt="Logo">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="producten.php">Producten</a></li>
+                <li><a href="#">Evenementen</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Over Ons</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+        <!-- Slideshow -->
+        </section>
+        <section id="original">
+            <!-- Original -->
+            <img src="images/original.png" alt="Original blikje">
+            <h1>Productinformatie</h1>
+            <p>
+               Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
+               sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
+               vitamin B12, artificial flavour</p>
+            <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
+        </section id="blueberry">
+        <section>
+            <!-- blueberry -->
+            <img src="images/blueberry.png" alt="Blueberry blikje">
+            <h1>Productinformatie</h1>
+            <p>Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
+               sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
+               vitamin B12, artificial flavour, blueberry flavour</p>
+            <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
+        </section>
+        <section id="citroen">
+            <!-- Citroen -->
+            <img src="images/citroen.png" alt="citroen blikje">
+            <h1>Productinformatie</h1>
+            <p>Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
+               sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
+               vitamin B12, artificial flavour, lemon flavour</p>
+               <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
+        </section>
+        <section id="aardbei">
+            <!-- Aardbei -->
+            <img src="images/aardbei.png" alt="aardbei blikje">
+            <h1>Productinformatie</h1>
+            <p>Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
+               sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
+               vitamin B12, artificial flavour, strawberry flavour</p>
+               <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
+        </section>
+    </main>
+</body>
+</html>
