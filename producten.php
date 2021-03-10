01b5ca27e37a71f122daab2818df2dae73cfa94f
daanberg <6013014@mborijnland.nl>
2021-03-10 17:20:12 +0100

    Producten big screen af

--- producten.php
+++ producten.php
@@ -12,9 +12,10 @@
 
 </head>
 <body>
-    <header>
+    <header id="nav-bar-big">
         <nav>
             <img src="images/logo.png" alt="Logo">
+        <section id="navbar">
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="producten.php">Producten</a></li>
@@ -22,27 +23,45 @@
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Over Ons</a></li>
             </ul>
+            </section>
+            <section id="navbar">
+            <details>
+                <summary>
+                    <h1>☰</h1>
+                </summary>
+                <nav id="item-nav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="producten.php">Producten</a></li>
+                        <li><a href="#">Evenementen</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Over Ons</a></li>
+                    </ul>
+                    </nav>
+            </details>
+            </section>
         </nav>
     </header>
     <main>
-        <section>
-        <!-- Slideshow -->
+        
+        <section id="slideshow">
+        <img src="images/wocky_slus-removebg-preview.png" alt="blikjes">
         </section>
         <section id="original">
             <!-- Original -->
             <img src="images/original.png" alt="Original blikje">
-            <h1>Productinformatie</h1>
-            <p>
+            <h1 class="titel">Original</h1>
+            <p class="container">
                Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
                sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
                vitamin B12, artificial flavour</p>
             <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
-        </section id="blueberry">
-        <section>
+        </section>
+        <section id="blueberry">
             <!-- blueberry -->
             <img src="images/blueberry.png" alt="Blueberry blikje">
-            <h1>Productinformatie</h1>
-            <p>Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
+            <h1 class="titel">Blauwe Bes</h1>
+            <p class="container">Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
                sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
                vitamin B12, artificial flavour, blueberry flavour</p>
             <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
@@ -50,8 +69,8 @@
         <section id="citroen">
             <!-- Citroen -->
             <img src="images/citroen.png" alt="citroen blikje">
-            <h1>Productinformatie</h1>
-            <p>Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
+            <h1 class="titel">Citroen</h1>
+            <p class="container">Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
                sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
                vitamin B12, artificial flavour, lemon flavour</p>
                <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
@@ -59,12 +78,14 @@
         <section id="aardbei">
             <!-- Aardbei -->
             <img src="images/aardbei.png" alt="aardbei blikje">
-            <h1>Productinformatie</h1>
-            <p>Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
+            <h1 class="titel">Aardbei</h1>
+            <p class="container">Ingrediënten: <br> Carbonated water, sugar, glucose-fructose, citric acid, taurine, 
                sodium bicarbonate, magnesium carbonate, caffeine, niacin, panthothenate, vitamin B6, 
                vitamin B12, artificial flavour, strawberry flavour</p>
                <img src="images/voedingswaarden.png" alt="tabel voedingswaardes">
         </section>
     </main>
+    <article class="opacity"></article>
+    <footer></footer>
 </body>
 </html>
