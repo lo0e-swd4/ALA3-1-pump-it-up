20cff673e2852f9fc9e5c9d593629faa6eac70f9
Ruurd <74601593+Lux42069@users.noreply.github.com>
2021-03-10 20:39:30 +0100

    Lux big screen 95% afgerond
    -html/java/css afgemaakt voor big screen

--- Home pagina/HOMEPHP.php
+++ Home pagina/HOMEPHP.php
@@ -22,24 +22,52 @@
 </div> 
 
 <section class="esport">
-<img src="./images/esport3" height="250px" alt="Foto esport">
-<img src="./images/esport1" height="250px" alt="Foto esport">
-<img src="./images/esport2" height="250px" alt="Foto esport">
+  <img src="./images/esport3.jpg" height="200px" alt="Foto esport">
+  <img src="./images/esport1.jpg" height="200px" alt="Foto esport">
+  <img src="./images/esport2.jpg" height="200px" alt="Foto esport">
+</section>
+
+<section class="kort1">
+  <img src="./images/event.jpg" height="250px" alt="Foto evenementen">
+  <img src="./images/blik.png" height="250px" alt="Foto producten">
+</section>
+
+<section class="kort2">
+  <img src="./images/contact.png" height="250px" alt="Foto contact">
+  <img src="./images/over ons.png" height="250px" width="465px" alt="Foto over ons">
 </section>
 
 <div class="backk" style="background-image: url('./images/backk.gif');"> 
 
+<div class="row">
+  <div class="column">
+    <p>Esport worden er met teams gespeeld, dus winnen doe je samen.</p>
+  </div>
+  <div class="column">
+    <p>Haal de ultimieme gamer uit jezelf tijdens gamen met Pump-it-Up!</p>
+  </div>
+   <div class="column">
+    <p>Samen voor plaats nummer 1! bewijs jezelf met Pump-it-Up!</p>
+  </div>
+</div>
+
 <p class="Openings">Pump-It-Up is een energie drank van gamers voor gamers</p>
-
-
 
 <div class="navbalk"></div>
   <div class="Slides" style="max-width:500px">
   <img class="mySlides" src="./images/doggo.jpg" style="width:100%">
-  <img class="mySlides" src="img_ny.jpg" style="width:100%">
+  <img class="mySlides" src="./images/soca.jpg" style="width:100%">
   <img class="mySlides" src="img_chicago.jpg" style="width:100%">
 </div>
 
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+<p class="eventtext">Onze energie dranken bevatten de hoogste kwaliteit ingredienten. <a href="#home">Lees hier meer!</a></p>
+<p class="producttext">Pump-it-Up organiseert esport events voor jong en oud! <a href="#home">Lees hier meer!</a></p>
+<p class="contacttext">Vragen, opmerkingen of hulp nodig? druk <a href="#home">hier</a> om naar ons vragen formulier te gaan!</p>
+<p class="overonstext">Wij zijn Pump-it-Up, een jong fris bedrijf vol creative werknemers...  <a href="#home">Lees hier meer!</a> </p>
 
 
 <script>
