28d0771f4ec5ed76c4059f3085266390d53a8f7b
Timmety1 <74976881+Timmety1@users.noreply.github.com>
2021-03-11 00:32:56 +0100

    Evenementen Toegevoegd

---
+++ evenementen.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Daan Berg">
+    <meta name="keywords" content="">
+    <title>Producten | Pump It Up</title>
+
+    <link rel="stylesheet" type="text/css" href="CSS/evenementen.css">  
+
+</head>
+<body>
+    <header id="nav-bar-big">
+        <nav>
+            <img src="images/logo.png" alt="Logo">
+        <section id="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="producten.php">Producten</a></li>
+                <li><a href="#">Evenementen</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Over Ons</a></li>
+            </ul>
+            </section>
+            <section id="navbar">
+            <details>
+                <summary>
+                    <h1>☰</h1>
+                </summary>
+                <nav id="item-nav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="producten.php">Producten</a></li>
+                        <li><a href="#">Evenementen</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Over Ons</a></li>
+                    </ul>
+                    </nav>
+            </details>
+            </section>
+        </nav>
+    </header>
+
+    <main>
+        <section id="about-events">
+            <h1>Over Evenementen
+            </h1>
+                <p>lorem ipsum is simply dummy text of the printing and typesetting 
+                    industry. lorem ipsum has been the industry's standard dummy 
+                    text ever since the 1500s, when an unknown printer took a galley of 
+                    type and scrambled it to make a type specimen book. it has 
+                    survived not only five centuries, but also the leap into electronic 
+                    typesetting, remaining essentially unchanged. it was popularised in 
+                    the 1960s with the release of letraset sheets containing lorem 
+                    ipsum passages, and more recently with desktop publishing 
+                    software like aldus pagemaker including versions of lorem ipsum
+            </p>
+
+        </section>
+
+        <article class="artiesten">
+        <h1>Artiesten
+</h1>
+            <article class="container">
+                <section class="box-item item-1">
+                    <img src="images/snelle.jpg">
+                    <h1>Snelle </h1>
+                    <p> Hier komt tekst</p>
+                </section>
+                <section class="box-item item-2">
+                    <img src="images/snelle.jpg">
+                    <h1>Rapper Sjors </h1>
+                    <p> Hier komt tekst</p>
+                    
+                </section>
+                <section class="box-item item-2">
+                    <img src="images/snelle.jpg">
+                    <h1>Lijpe</h1>
+                    <p> Hier komt tekst</p>
+                </section>
+                
+            </article>
+        </article>
+
+       
+    </main>
+    <footer></footer>
+</body>
+
+</html>
